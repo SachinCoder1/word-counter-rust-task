2e64f8d49534b8a596ad30abbbf3c40bae68cecf
Sachin Gurjar <98685642+SachinCoder1@users.noreply.github.com>
2024-01-16 12:12:26 +0530

    task complete

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+struct WordCounter {
+    text: String,
+}
+
+impl WordCounter {
+    fn new(text: &str) -> WordCounter {
+        WordCounter {
+            text: text.to_string(),
+        }
+    }
+
+    fn count_words(&self) -> usize {
+        self.text.split_whitespace().count()
+    }
+}
+
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Please enter some text:");
+    let mut input_text = String::new();
+    io::stdin()
+        .read_line(&mut input_text)
+        .expect("Failed to read line");
+
+    let counter = WordCounter::new(&input_text);
+    let word_count = counter.count_words();
+    println!("Word count: {}", word_count);
 }
